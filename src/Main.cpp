412f208ea1c0f21c84936e4b5ace71edecb6f038
dreamsdestroyer <copies.everywhere@gmail.com>
2024-07-23 01:40:08 +0300

    verison 0.1

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,31 +9,50 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 800), "Snake Online");
-
     Grid grid(ROWS, COLUMNS);
     grid.initializeGrid();
     Snake snake(grid);
     Food food(grid);
     sf::Clock clock;
     sf::Sprite sprite;
-    
-    const sf::Time timePerFrame = sf::seconds(0.1f);
-    while (window.isOpen())
-    {
+    snake.growSnake();
+    snake.growSnake();
+    const sf::Time timePerFrame = sf::seconds(1.0f);
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
-            switch (event.type) {
-            
+        }
+
+        // Handle user input for snake direction
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            if (snake.getDirection().x == 0) {
+                snake.setDirection(sf::Vector2i(-1, 0));
             }
         }
-        window.clear();
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            if (snake.getDirection().x == 0) {
+                snake.setDirection(sf::Vector2i(1, 0));
+            }
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            if (snake.getDirection().y == 0) {
+                snake.setDirection(sf::Vector2i(0, -1));
+            }
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            if (snake.getDirection().y == 0) {
+                snake.setDirection(sf::Vector2i(0, 1));
+            }
+        }
+
         if (clock.getElapsedTime() >= timePerFrame) {
             snake.moveSnake();
             clock.restart();
         }
+
+        window.clear();
         grid.drawGrid(window);
         window.draw(food.getSprite());
         snake.drawSnake(window);
--- src/Snake.cpp
+++ src/Snake.cpp
@@ -1,12 +1,15 @@
 #include "Snake.h"
 
-Snake::Snake(Grid& grid) {
+Snake::Snake(Grid& grid)
+{
     ptrGrid = &grid;
     snake.emplace_back(std::make_shared<Entity>(SNAKE_HEAD_UP));
     snake[0].get()->setSpritePosition(ptrGrid->getGrid()[4][2].get()->getSprite().getPosition());
     sf::Vector2f previousSprite = snake.back().get()->getSprite().getPosition();
     snake.emplace_back(std::make_shared<Entity>(SNAKE_TAIL_DOWN));
     snake.back().get()->setSpritePosition(sf::Vector2f(previousSprite.x, previousSprite.y + 40));
+    direction = sf::Vector2i(0,1);
+    globalPosition = sf::Vector2i(8, 8);
 }
 
 std::vector<std::shared_ptr<Entity>>& Snake::getVectorSnake() {
@@ -19,6 +22,7 @@ void Snake::growSnake() {
     previousSnakeEntity.get()->setSpriteTexture(SNAKE_BODY_VERTICAL);
     snake.back().get()->setSpritePosition(sf::Vector2f(previousSnakeEntity.get()->getSprite().getPosition().x,
      previousSnakeEntity.get()->getSprite().getPosition().y + 40));
+
 }
 
 void Snake::drawSnake(sf::RenderWindow& window) {
@@ -27,11 +31,25 @@ void Snake::drawSnake(sf::RenderWindow& window) {
     }
 }
 
+void Snake::setDirection(const sf::Vector2i& otherDirection) {
+    direction = otherDirection;
+}
+
+const sf::Vector2i& Snake::getDirection() {
+    return direction;
+}
+
 void Snake::moveSnake() {
     // direction
-    // 
-
-    std::cout << "hello" << std::endl;
+    snake[0].get()->setSpritePosition(ptrGrid->getGrid()[globalPosition.x + direction.x][globalPosition.y + direction.y].get()->getSprite().getPosition());
+    globalPosition += direction;
+    sf::Vector2i previousPosition(globalPosition);
+    for (int i = 1; i < snake.size(); i++) {
+        snake[i].get()->setSpritePosition(sf::Vector2f(ptrGrid->getGrid()[previousPosition.x - direction.x][previousPosition.y - direction.y].get()->getSprite().getPosition().x,
+            ptrGrid->getGrid()[previousPosition.x - direction.x][previousPosition.y - direction.y].get()->getSprite().getPosition().y));
+        previousPosition = sf::Vector2i(previousPosition.x - direction.x, previousPosition.y - direction.y);
+    }
+    std::cout << snake[0].get()->getSprite().getPosition().x << " " << snake[0].get()->getSprite().getPosition().y << std::endl;
 }
 
 bool Snake::eatSnake(Food& food) {
